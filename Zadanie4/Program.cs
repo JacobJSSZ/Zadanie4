de49b6f4aae636e1661cab83effe9ba5ccdb4966
Jakub <imraszun@gmail.com>
2023-09-28 16:09:48 +0200

    Dodaj pliki projektów.

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,16 @@
+﻿var name = "Ewa";
+var gender = "Kobieta";
+var age = 33;
+
+if (gender == "Kobieta" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (age < 30 && gender == "Kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (gender != "Kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
